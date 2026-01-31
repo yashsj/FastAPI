f05c760451ccbe0e78878fd1c228f6f11aedeb50
Suyash <Suyashsj@hotmail.com>
2026-01-30 19:03:39 -0600

    Add PUT endpoint to update books

--- books.py
+++ books.py
@@ -1,15 +1,19 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+
 
 app = FastAPI()
 
 
+
 BOOKS=[
-    {'title':'title1','author':'author1'},
-    {'title':'title2','author':'author2'},
-    {'title':'title3','author':'author2'},
-    {'title':'title4','author':'author4'},
-    {'title':'title5','author':'author5'},
-    {'title':'title6','author':'author6'},
+    {'title':'title1','author':'author1','category':'science'},
+    {'title':'title2','author':'author2','category':'history'},
+    {'title':'title3','author':'author3','category':'math'},
+    {'title':'title3','author':'author2','category':'math'},
+    {'title':'title4','author':'author4','category':'history'},
+    {'title':'title5','author':'author5','category':'math'},
+    {'title':'title5','author':'author5','category':'history'},
+    {'title':'title6','author':'author6','category':'math'}
 ]
 @app.get("/books")
 async def read_all_books():
@@ -45,3 +49,16 @@ async def read_author_and_title(author: str, title: str):
         ):
             books_to_return.append(book)
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i]["title"] = updated_book
+ç
