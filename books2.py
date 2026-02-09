ee2fc3c4cc58f8c840eb80aebfab7100bbfce290
Suyash <yashsj@yahoo.co.in>
2026-02-08 20:53:15 -0600

    Implement book update and delete endpoints

--- books2.py
+++ books2.py
@@ -11,13 +11,15 @@ class Book:
     author:str
     description:str
     rating:int
+    published_date:int
 
-    def __init__(self, id:int, title:str, author:str, description:str, rating:int):
+    def __init__(self, id:int, title:str, author:str, description:str, rating:int, published_date:int):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 class BookRequest(BaseModel):
     id:Optional[int]=Field(description='ID is not needed on creation',default=None)
@@ -25,6 +27,7 @@ class BookRequest(BaseModel):
     author:str=Field(min_length=1)
     description:str=Field(min_length=3,max_length=100)
     rating:int=Field(gt=-1,lt=6)
+    published_date:int=Field(gt=0)
 
     model_config = {
         "json_schema_extra":{
@@ -33,6 +36,7 @@ class BookRequest(BaseModel):
                 "author":"Eric",
                 "description":"A new book",
                 "rating":1,
+                "published_date":2000
             }
         }
     }
@@ -40,16 +44,26 @@ class BookRequest(BaseModel):
 
 
 BOOKS =[
-    Book(1,"Computer science","Eric","An excellent book",5),
-    Book(2,"Computer science-II","Eric Roby","An excellent book and great",5),
-    Book(3,"Database","Eric Ling","An okay book",3),
-    Book(4,"Data structures","Mathew Linddg","A book that made me cry",2),
-    Book(5,"Personality developement","Ash","A great book",4)
+    Book(1,"Computer science","Eric","An excellent book",5,2000),
+    Book(2,"Computer science-II","Eric Roby","An excellent book and great",5,2011),
+    Book(3,"Database","Eric Ling","An okay book",3,2012),
+    Book(4,"Data structures","Mathew Linddg","A book that made me cry",2,2005),
+    Book(5,"Personality developement","Ash","A great book",4,2004)
 ]
 
 @app.get("/books")
 async def read_books():
     return BOOKS
+
+@app.get("/books/publish/")
+async def get_book_by_date(published_date:int):
+    books_to_return=[]
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
 
 
 @app.get("/books/{book_id}")
@@ -95,6 +109,12 @@ async def update_book_by_body(book:BookRequest):
             BOOKS[i]=book
 
 
+@app.delete("/books/{book_id}")
+async def delete_book(book_id:int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            del BOOKS[i]
+            break
 
 
 
